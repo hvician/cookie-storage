45b2f4870ee5c98c401fcd9f310adffdb4041fed
zerosignal <no@name.com>
2020-04-01 18:40:41 -0400

    prettier formating

--- src/format-cookie.ts
+++ src/format-cookie.ts
@@ -3,7 +3,8 @@ import { CookieOptions } from './cookie-options';
 const getSameSiteValue = (o: CookieOptions): string | null => {
   const { sameSite } = o;
   if (typeof sameSite === 'undefined') return null;
-  if (['lax', 'strict', 'none'].indexOf(sameSite.toLowerCase()) >= 0) return sameSite;
+  if (['lax', 'strict', 'none'].indexOf(sameSite.toLowerCase()) >= 0)
+    return sameSite;
   return null;
 };
 
